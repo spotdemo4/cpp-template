7df8a54ebfbb4fbc4f23eaad2d63c0f1b7e338eb
trev <me@trev.xyz>
2026-05-11 10:19:40 -0400

    init

---
+++ README.md
@@ -0,0 +1,39 @@
+# template
+
+[![check](https://img.shields.io/github/actions/workflow/status/spotdemo4/template/check.yaml?branch=main&logo=github&logoColor=%23bac2de&label=check&labelColor=%23313244)](https://github.com/spotdemo4/template/actions/workflows/check.yaml)
+[![vulnerable](https://img.shields.io/github/actions/workflow/status/spotdemo4/template/vulnerable.yaml?branch=main&logo=github&logoColor=%23bac2de&label=vulnerable&labelColor=%23313244)](https://github.com/spotdemo4/template/actions/workflows/vulnerable.yaml)
+[![flakehub](https://img.shields.io/endpoint?url=https://flakehub.com/f/spotdemo4/template/badge&labelColor=%23313244)](https://flakehub.com/flake/spotdemo4/template)
+
+default template for projects
+
+part of [spotdemo4/templates](https://github.com/spotdemo4/templates)
+
+## requirements
+
+- [nix](https://nixos.org/)
+
+## getting started
+
+```elm
+nix develop
+```
+
+### format
+
+```elm
+nix fmt
+```
+
+### check
+
+```elm
+nix flake check
+```
+
+### release
+
+```elm
+bumper
+```
+
+releases are automatically created for [significant](https://www.conventionalcommits.org/en/v1.0.0/#summary) changes
---
+++ flake.nix
@@ -0,0 +1,139 @@
+{
+  description = "template";
+
+  nixConfig = {
+    extra-substituters = [
+      "https://nix.trev.zip"
+    ];
+    extra-trusted-public-keys = [
+      "trev:I39N/EsnHkvfmsbx8RUW+ia5dOzojTQNCTzKYij1chU="
+    ];
+  };
+
+  inputs = {
+    systems.url = "github:spotdemo4/systems";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    trevpkgs = {
+      url = "github:spotdemo4/trevpkgs";
+      inputs.systems.follows = "systems";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      trevpkgs,
+      ...
+    }:
+    trevpkgs.libs.mkFlake (
+      system: pkgs: {
+
+        # nix develop [#...]
+        devShells = {
+          default = pkgs.mkShell {
+            shellHook = pkgs.shellhook.ref;
+            packages = with pkgs; [
+              # c++
+              clang-tools
+              cmake
+
+              # lint
+              nixd
+              nil
+
+              # format
+              nixfmt
+              oxfmt
+              treefmt
+
+              # util
+              bumper
+            ];
+          };
+
+          bump = pkgs.mkShell {
+            packages = with pkgs; [
+              bumper
+            ];
+          };
+
+          release = pkgs.mkShell {
+            packages = with pkgs; [
+              flake-release
+            ];
+          };
+
+          update = pkgs.mkShell {
+            packages = with pkgs; [
+              renovate
+            ];
+          };
+
+          vulnerable = pkgs.mkShell {
+            packages = with pkgs; [
+              flake-checker # nix
+              zizmor # actions
+            ];
+          };
+        };
+
+        # nix fmt
+        formatter = pkgs.treefmt.withConfig {
+          configFile = ./treefmt.toml;
+          runtimeInputs = with pkgs; [
+            nixfmt
+            oxfmt
+          ];
+        };
+
+        # nix flake check
+        checks = pkgs.mkChecks {
+          nix = {
+            root = ./.;
+            filter = file: file.hasExt "nix";
+            packages = with pkgs; [
+              nixfmt
+            ];
+            script = ''
+              nixfmt --check "$file"
+            '';
+          };
+
+          actions = {
+            root = ./.github/workflows;
+            filter = file: file.hasExt "yaml";
+            packages = with pkgs; [
+              action-validator
+              zizmor
+            ];
+            script = ''
+              action-validator "$file"
+              zizmor --offline "$file"
+            '';
+          };
+
+          renovate = {
+            root = ./.github;
+            files = ./.github/renovate.json;
+            packages = with pkgs; [
+              renovate
+            ];
+            script = ''
+              renovate-config-validator renovate.json
+            '';
+          };
+
+          config = {
+            root = ./.;
+            filter = file: file.hasExt "json" || file.hasExt "yaml" || file.hasExt "toml" || file.hasExt "md";
+            packages = with pkgs; [
+              oxfmt
+            ];
+            script = ''
+              oxfmt --check
+            '';
+          };
+        };
+      }
+    );
+}
