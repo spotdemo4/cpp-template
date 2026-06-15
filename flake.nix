f5bf43744fc6cf9d29dee725cded09e2ebe13c65
spotdemo4 <me@trev.xyz>
2026-06-15 12:08:09 -0400

    ci: validate forgejo workflows

--- flake.nix
+++ flake.nix
@@ -200,9 +200,11 @@
             root = ./.forgejo/workflows;
             filter = file: file.hasExt "yaml";
             packages = with pkgs; [
+              forgejo-runner
               zizmor
             ];
             script = ''
+              forgejo-runner validate --workflow --path "$file"
               zizmor --offline "$file"
             '';
           };
