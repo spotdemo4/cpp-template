5a5473732e1d4b0a9063dc8da7832023b85f4a96
doctor <doctor@trev.xyz>
2026-07-13 22:02:39 +0000

    bump: v0.2.2 -> v0.3.0

--- flake.nix
+++ flake.nix
@@ -91,7 +91,7 @@
           default = pkgs.stdenv.mkDerivation (
             final: with pkgs.lib; {
               pname = "cpp-template";
-              version = "0.2.2";
+              version = "0.3.0";
 
               src = fileset.toSource {
                 root = ./.;
